4f295b31d4fc9a9274bf607eff9e6635dc90c9ef
BernhardWeiss <bernhard.weissenbach@outlook.com>
2023-06-12 16:35:58 +0200

    removed old methods from plot.py and added docstrings

--- wordstream/plot.py
+++ wordstream/plot.py
@@ -6,65 +6,16 @@ import pandas as pd
 from bokeh.models import LegendItem, GlyphRenderer, Axis, DatetimeTickFormatter, DatetimeTicker, LinearAxis, Grid, MultiLine
 
 from wordstream.boxes import build_boxes
-from wordstream.draw import draw_fact_check, DrawOptions, draw_parlament
+from wordstream.draw import DrawOptions, draw_parlament
 
 font_path = Path("../fonts/RobotoMono-VariableFont_wght.ttf")
+"""Path where font for WordStreamVis is stored """
 
 s = 43
 random.seed(s)
 np.random.seed(s)
 
-
-def pt_to_px(ppi, pt: float) -> float:
-    # 1 pt = 1/72 inches
-    inches = pt / 72
-    return inches * ppi
-
-
-def px_to_pt(ppi, px: int) -> float:
-    inches = px / ppi
-    return inches * 72
-
-
-assert pt_to_px(100, px_to_pt(100, 10)) == 10
-
-
-def font_size_conversion(ppi, font_size, ppi_base=72):
-    """ I think the libraries use  """
-    px = pt_to_px(ppi, font_size)
-    pt = px_to_pt(ppi_base, int(px))
-    return pt
-
-
-def plot_matplotlib():
-    import matplotlib.pyplot as plt
-    import matplotlib
-
-    options = DrawOptions(width=24, height=12, min_font_size=15, max_font_size=45)
-    placements = draw_fact_check(options)
-    topics = list(placements.keys())
-
-    # matplotlib.use("svg", force=True)
-
-    ppi = 72
-    fig, ax = plt.subplots(1, 1, figsize=(options.width, options.height))
-    for topic, col in zip(topics, ["red", "green", "blue", "purple"]):
-        for word in placements[topic]:
-            # apparently matplotlib also uses pixels? use ppi of plot to calculate pixels
-            font_size = pt_to_px(ppi, word["font_size"])
-            # font_size = word["font_size"]
-            ax.text(x=word["x"], y=word["y"], s=word["text"], color=col, font=font_path, fontsize=font_size, ha="left",
-                    va="top")
-            ax.plot(word["x"], word["y"], 'ro')
-
-    ax.set_xlim(0, options.width)
-    ax.set_ylim(options.height / 2, -options.height / 2)
-
-    plt.show(dpi=ppi)
-    # plt.savefig("../../mat_img.svg")
-
-
-party_col = {
+PARTY_COLOR = {
     "F": "blue",
     "G": "limegreen",
     "L": "gold",
@@ -76,8 +27,9 @@ party_col = {
     "N": "magenta",
     "PJ": "grey"
 }
+"""Color Mapping of fractions"""
 
-party_name = {
+PARTY_NAME = {
     "F": "FPÖ",
     "G": "Grüne",
     "L": "Liberales Forum",
@@ -89,9 +41,34 @@ party_name = {
     "N": "NEOS",
     "PJ": "PILZ/JETZT"
 }
+"""Full name of fractions"""
+
+
+def pt_to_px(ppi, pt: float) -> float:
+    """Converts pt to pixel"""
+    # 1 pt = 1/72 inches
+    inches = pt / 72
+    return inches * ppi
+
+
+def px_to_pt(ppi, px: int) -> float:
+    """Convert pixel to pt"""
+    inches = px / ppi
+    return inches * 72
+
+
+assert pt_to_px(100, px_to_pt(100, 10)) == 10
+
 
 
 def plot_bokeh(options: DrawOptions, legislative_periods: list[str], fulltext: bool = False) -> tuple[str,str]:
+    """Plats WordStream visualisation in bokeh and generates standalone html document
+
+    Keyword arguments:
+    options -- DrawOptions for bokeh plot
+    legislative_periods -- roman numerals of periods which should be plotted
+    fulltext -- true: fulltext of motions is used, false: eurovoc keywords are used
+    """
 
     from bokeh.io import curdoc, show
     from bokeh.models import ColumnDataSource, Plot, Text, Range1d, HoverTool, Circle, Legend, FixedTicker
@@ -189,13 +166,13 @@ def plot_bokeh(options: DrawOptions, legislative_periods: list[str], fulltext: b
 
     topic_glyphs = dict()
     for topic in topics:
-        col = party_col[topic]
+        col = PARTY_COLOR[topic]
         df = pd.DataFrame.from_records(placements[topic])
         if df.size == 0:
             # No topics of club could be placed
             continue
         df["font_size"] = df["font_size"].apply(lambda v: f"{pt_to_px(ppi, v)}px")
-        df["topic"] = party_name[topic]
+        df["topic"] = PARTY_NAME[topic]
         df["col"] = col
 
         glyph = Text(x="x", y="y", text="text", text_color=col, text_font_size="fs", text_baseline="top",
@@ -209,7 +186,7 @@ def plot_bokeh(options: DrawOptions, legislative_periods: list[str], fulltext: b
         ds_legend = ColumnDataSource(dict(col=df.col, x=df.x, y=df.y + 100))
         points = Circle(x="x", y="y", fill_color="col")
         legend_renderer = plot.add_glyph(ds_legend, points)
-        topic_glyphs[party_name[topic]] = legend_renderer
+        topic_glyphs[PARTY_NAME[topic]] = legend_renderer
 
 
     legend = Legend(
